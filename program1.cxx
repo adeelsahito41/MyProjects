21ace3daf285c27214934965f61cf129a1b62c90
adeelsahito41 <adeelsahito5566@gmail.com>
2024-10-24 18:45:00 +0500

    Add files via upload
    Write a C program that read two integers from the keyboard and store the value entered in the variable a & b and display their sum.

---
+++ program1.cxx
@@ -0,0 +1,14 @@
+// 24AI-41
+
+#include<stdio.h>
+int main(){
+	int a;
+	int b;
+	int sum;
+	printf("Enter value for a: ");
+	scanf("%d", &a);
+	printf("Enter value for b: ");
+	scanf("%d", &b);
+	sum = a + b;
+	printf("%d", sum);
+}
